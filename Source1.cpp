3aac4453b78c891de033e888b34b2a5fe6e36f9b
BorisovetsGleb <92142560+BorisovetsGleb@users.noreply.github.com>
2022-03-10 09:36:51 +0300

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -9,10 +9,51 @@ using namespace std;
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+int Speed = 15;
+int Level = 1;
+
 
 SDL_Window* win = NULL;
 SDL_Surface* scr = NULL;
 SDL_Surface* flower = NULL;
+SDL_Surface* level = NULL;
+
+//Level 1; перевернут на бок (/*не знаю, как исправить*/ так и должно быть)
+//ID: 1-wall up and down, 2-wall left and right, 0-ground, 3, 4, 5, 6-wall-ygol
+vector<vector<int>>Level1 = {
+		{3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 4},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+		{5, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 6}
+};
+
+vector <const char*> ground = { "Ground1.png", "Ground2.png", "Ground3.png", "Ground1.png", "Ground4.png", "Ground5.png", "Ground1.png" };
 
 /*void line(float x1, float y1, float x2, float y2, SDL_Renderer* ren)
 {
@@ -67,7 +108,7 @@ bool init() {
 bool load() {
 	//flower = IMG_LoadGIF_RW(C:\\Users\\Honor\\Desktop\\Character1.gif);
 	//flower = IMG_LoadGIF("C:\Users\Honor\Desktop\Character1.gif");
-	flower = IMG_Load("C:\\Users\\Honor\\Desktop\\Character1.gif");
+	flower = IMG_Load("C:\\Users\\Honor\\Desktop\\Character1.png");
 	if (flower == NULL) {
 		std::cout << "Can't load: " << IMG_GetError() << std::endl;
 		//return false;
@@ -92,8 +133,93 @@ void quit() {
 	IMG_Quit();
 }
 
+//Load level
+
+vector <SDL_Surface> sprites = {};
+void LoadLevel(int levelID)
+{
+	SDL_Rect TilePlace;
+	TilePlace.w = 55;
+	TilePlace.h = 55;
+	if (levelID == 1)
+	{
+		for (int x1 = 0; x1 < 30; x1++)
+		{
+			for (int y1 = 0; y1 < 20; y1++)
+			{
+				TilePlace.x = x1 * 50 + 25;
+				TilePlace.y = y1 * 50 + 25;
+
+				if (Level1[x1][y1] == 1)
+				{
+					level = IMG_Load(ground[rand()%7]);
+					SDL_BlitScaled(level, NULL, scr, &TilePlace);
+				}
+				if (Level1[x1][y1] == 2)
+				{
+					level = IMG_Load("Wall1.png");
+					SDL_BlitScaled(level, NULL, scr, &TilePlace);
+				}
+				if (Level1[x1][y1] == 3)
+				{
+					level = IMG_Load("WallYgol1.png");
+					SDL_BlitScaled(level, NULL, scr, &TilePlace);
+				}
+				if (Level1[x1][y1] == 4)
+				{
+					level = IMG_Load("WallYgol4.png");
+					SDL_BlitScaled(level, NULL, scr, &TilePlace);
+				}
+				if (Level1[x1][y1] == 5)
+				{
+					level = IMG_Load("WallYgol2.png");
+					SDL_BlitScaled(level, NULL, scr, &TilePlace);
+				}
+				if (Level1[x1][y1] == 6)
+				{
+					level = IMG_Load("WallYgol3.png");
+					SDL_BlitScaled(level, NULL, scr, &TilePlace);
+				}
+				if (Level1[x1][y1] == 7)
+				{
+					level = IMG_Load("Wall4.png");
+					SDL_BlitScaled(level, NULL, scr, &TilePlace);
+				}
+			}
+		}
+	}
+}
+
+void Enemy()
+{
+
+}
+
+//Can player move?
+bool CanUp(int y)
+{
+	if (y < 75) { return false; }
+	return true;
+}
+bool CanDown(int y)
+{
+	if (y > 900) { return false; }
+	return true;
+}
+bool CanLeft(int x)
+{
+	if (x < 75) { return false; }
+	return true;
+}
+bool CanRight(int x)
+{
+	if (x > 1400) { return false; }
+	return true;
+}
+
 int main(int argc, char* args[])
 {
+	//int level = 1;
 	int ttime = 0;
 	if (!init()) {
 		system("pause");
@@ -108,17 +234,17 @@ int main(int argc, char* args[])
 	}
 
 	SDL_Rect bg_Player;
-	bg_Player.w = 60;
-	bg_Player.h = 60;
-	bg_Player.x = 0;
-	bg_Player.y = 0;
+	bg_Player.w = 80;
+	bg_Player.h = 80;
+	bg_Player.x = 350;
+	bg_Player.y = 350;
 
-	int x = 0, y = 0;
+	int x = 350, y = 350;
 
 	bool IsRun = 1;
 	SDL_Event event;
 
-	vector <const char *> sprites = { "C:\\Users\\Honor\\Desktop\\Character1.png", "C:\\Users\\Honor\\Desktop\\Character2.png" };
+	vector <const char *> sprites = { "Character1.png", "Character2.png" };
 
 	while (IsRun)
 	{
@@ -127,43 +253,39 @@ int main(int argc, char* args[])
 			//if (event.type = SDL_QUIT & ttime > 10) { IsRun = 0; }
 			if (event.type = SDL_KEYDOWN)
 			{
+				//Move
 				if (event.key.keysym.sym == SDLK_w)
 				{
-					if (y > 2) { y = y - 2; }
+					if (CanUp(y)) { y = y - Speed; }
 				}
 				if (event.key.keysym.sym == SDLK_s)
 				{
-					y = y + 2;
+					if (CanDown(y)) { y = y + Speed; }
 				}
 				if (event.key.keysym.sym == SDLK_a)
 				{
-					if (x > 2) { x = x - 2; }
+					if (CanLeft(x)) { x = x - Speed; }
 				}
 				if (event.key.keysym.sym == SDLK_d)
 				{
-					x = x + 2;
+					if (CanRight(x)) { x = x + Speed; }
 				}
 			}
 		}
+
+		SDL_FillRect(scr, NULL, SDL_MapRGB(scr->format, 0, 0, 0));
+
+		LoadLevel(Level);
 
 		bg_Player.x = x;
 		bg_Player.y = y;
 
 		ttime++;
-		flower = IMG_Load(sprites[(ttime / 500) % 2]);
-		SDL_FillRect(scr, NULL, SDL_MapRGB(scr->format, 0, 0, 0));
+		flower = IMG_Load(sprites[(ttime / 10) % 2]);
 		SDL_BlitScaled(flower, NULL, scr, &bg_Player);
 		SDL_UpdateWindowSurface(win);
 	}
 
-	//SDL_BlitScaled(flower, NULL, scr, &bg_flower);
-
-	//SDL_UpdateWindowSurface(win);
-
-
-	//SDL_Delay(2000);
-
 	quit();
-
 	return 0;
 }
