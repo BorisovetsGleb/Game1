31aaa483bb1103500c5f427452321af122b51aee
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2022-03-17 17:30:39 +0300

    Add files via upload

--- GeneratingLevelsCodeBlocks/Player.cpp
+++ GeneratingLevelsCodeBlocks/Player.cpp
@@ -1,7 +1,9 @@
-#include "Object.h"
-
+#include "Player.h"
+#include "Extern.h"
+#include "Level.h"
 int errorX = 0;
 int errorY = 0;
+vector<Player*> players = {};
 
 Player::Player(int x, int y, int width, int height, int speed){
     this->x = x;//<-- maybe delete this
@@ -9,78 +11,75 @@ Player::Player(int x, int y, int width, int height, int speed){
     this->width = width;//<-- maybe delete this
     this->height = height;//<-- maybe delete this
     this->speed = speed;
-    gridX = x % 50;
-    gridY = y % 50;
-    rectangle.w = width;
-	rectangle.h = height;
-//	rectangle.x = x;
-//	rectangle.y = y;
-	rectanglePointer = &rectangle;
+    players.push_back(this);
+    gridX = x / 50;
+    gridY = y / 50;
+    rectanglePointer = new SDL_Rect;
+    rectanglePointer->w = width;
+	rectanglePointer->h = height;
     texturePointer = textures[0];
 }
 
-bool Player::CanUp()
-{
-    if (errorY < 10)
-    {
-        if (room1[gridX][gridY - 1] == 1 || room1[gridX][gridY - 1] == 2 || room1[gridX][gridY - 1] == 3 || room1[gridX][gridY - 1] == 4 || room1[gridX][gridY - 1] == 5 || room1[gridX][gridY - 1] == 6)
-            return false;
-    }
+bool Player::CanUp(){
+    //if (currentRoom->roomStructure[gridY - 1][gridX] == 1 || currentRoom->roomStructure[gridY - 1][gridX] == 2 || currentRoom->roomStructure[gridY - 1][gridX] == 3 || currentRoom->roomStructure[gridY - 1][gridX] == 4 || currentRoom->roomStructure[gridY - 1][gridX] == 5 || currentRoom->roomStructure[gridY - 1][gridX] == 6)
+    if (errorY < 70)
+        for(int i = 1; i <= 6; i++)
+            if(currentRoom->roomStructure[gridY - 1][gridX])
+                return false;
 	return true;
 }
-bool Player::CanDown()
-{
-    if (errorY < 10)
-    {
-        if (room1[gridX][gridY + 1] == 1 || room1[gridX][gridY + 1] == 2 || room1[gridX][gridY + 1] == 3 || room1[gridX][gridY + 1] == 4 || room1[gridX][gridY + 1] == 5 || room1[gridX][gridY + 1] == 6)
-            return false;
-    }
+bool Player::CanDown(){
+    //if (currentRoom->roomStructure[gridY + 1][gridX] == 1 || currentRoom->roomStructure[gridY + 1][gridX] == 2 || currentRoom->roomStructure[gridY + 1][gridX] == 3 || currentRoom->roomStructure[gridY + 1][gridX] == 4 || currentRoom->roomStructure[gridY + 1][gridX] == 5 || currentRoom->roomStructure[gridY + 1][gridX] == 6)
+    if (errorY == 110)
+        for(int i = 1; i <= 6; i++)
+            if(currentRoom->roomStructure[gridY + 1][gridX])
+                return false;
 	/*if (y > 900)
         return false;*/
 	return true;
 }
-bool Player::CanLeft()
-{
-	if (x < 75)
-        return false;
-	return true;
+bool Player::CanLeft(){
+    //if (currentRoom->roomStructure[gridY][gridX - 1] == 1 || currentRoom->roomStructure[gridY][gridX] == 1 || currentRoom->roomStructure[gridY][gridX - 1] == 2 || currentRoom->roomStructure[gridY][gridX - 1] == 3 || currentRoom->roomStructure[gridY][gridX - 1] == 4 || currentRoom->roomStructure[gridY][gridX - 1] == 5 || currentRoom->roomStructure[gridY][gridX - 1] == 6)
+    if(errorX == 111)
+        for(int i = 1; i <= 6; i++)
+            if(currentRoom->roomStructure[gridY][gridX - 1] == i ||
+               currentRoom->roomStructure[gridY][gridX])
+                return false;
+    return true;
 }
-bool Player::CanRight()
-{
-	if (x > 1400)
-        return false;
-	return true;
+bool Player::CanRight(){
+    //if (currentRoom->roomStructure[gridY][gridX + 1] == 1 || currentRoom->roomStructure[gridY][gridX + 1] == 2 || currentRoom->roomStructure[gridY][gridX + 1] == 3 || currentRoom->roomStructure[gridY][gridX + 1] == 4 || currentRoom->roomStructure[gridY][gridX + 1] == 5 || currentRoom->roomStructure[gridY][gridX + 1] == 6)
+    if(errorX == 96)
+        for(int i = 1; i <= 6; i++)
+            if(currentRoom->roomStructure[gridY][gridX + 1] ||
+               currentRoom->roomStructure[gridY][gridX])
+                return false;
+    return true;
 }
-
-
 
 void Player::Move(){
     //Move
     if (keyboardKeysPressed[SDL_SCANCODE_W]){
-        //cout << keyboardKeysPressed[SDL_SCANCODE_W];
-        //if (CanUp())
+        if (CanUp())
             y = y - speed;
     }
     if (keyboardKeysPressed[SDL_SCANCODE_S]){
-        //cout << keyboardKeysPressed[SDL_SCANCODE_S];
-        //if (CanDown())
+        if (CanDown())
             y = y + speed;
     }
     if (keyboardKeysPressed[SDL_SCANCODE_A]){
-        //cout << keyboardKeysPressed[SDL_SCANCODE_A];
-        //if (CanLeft())
+        if (CanLeft())
             x = x - speed;
     }
     if (keyboardKeysPressed[SDL_SCANCODE_D]){
-        //cout << keyboardKeysPressed[SDL_SCANCODE_D];
-        //if (CanRight())
+        if (CanRight())
             x = x + speed;
     }
     if(SDL_GetError() != NULL){
         //cout << SDL_GetError() <<endl;
     }
-    gridX = ((x + 40 - 50) / 50);
-    gridY = ((y + 40 - 50) / 50);
+    gridX = (x - 20) / 50;
+    gridY = (y - 20) / 50;
     if(gridX == 0){
         x += 1380;
         Level::SetRoom(currentRoom->exits[0]);
@@ -97,14 +96,12 @@ void Player::Move(){
         y -= 900;
         Level::SetRoom(currentRoom->exits[3]);
     }
-    else{
-    rectangle.x = x;
-    rectangle.y = y;
-    errorX = abs(x - (gridX * 50 * 50));
-    errorY = abs(y - (gridY * 50 + 50));
-//    cout << "gridX: " << gridX << endl;
-//    cout << "gridY: " << gridY << endl << endl;
-    }
+    rectanglePointer->x = x;
+    rectanglePointer->y = y;
+    errorX = abs((x + width / 2) - (gridX * 50 - 25));
+    errorY = abs((y + height / 2) - (gridY * 50 - 25));
+    cout << "ERRORS: \n" << errorX << endl << errorY << endl << endl;
     SDL_RenderCopy(renderer, texturePointer, NULL, rectanglePointer);
+
 }
 
